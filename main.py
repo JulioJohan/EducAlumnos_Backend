ab3cd29e30f82824a146225ad2595c4d593f01ab
JulioJohan <juliojohan10@gmail.com>
2023-12-04 22:57:11 -0600

    Cambiando base de datos

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import json
 
 app = Flask(__name__)
 CORS(app)  # Esto habilitará CORS para todas las rutas
-app.config["MONGO_URI"] = "mongodb+srv://Johan:Johan@dbloginreact.2ljvu.mongodb.net/EducAlumnos"
+app.config["MONGO_URI"] = "mongodb+srv://mariagmenr:laravel8@cluster0.u0z54ln.mongodb.net/Educalumnos"
 mongo = PyMongo(app)
 
 @app.route('/add_score', methods=['POST'])
 def add_score():
     data = request.get_json()
     print(data)
-    name = data['nombreJugador']
-    score = data['puntos']
-    mongo.db.nivel_dos.insert_one({'nombreJugador': name, 'puntos': score})
+    name = data['Nombre']
+    score = data['Puntuaje']
+    mongo.db.jugadores.insert_one({'Nombre': name, 'Puntuaje': score})
     return jsonify(message="Score added successfully"), 201
 
 @app.route('/top_scores', methods=['GET'])
 def get_top_scores():
-    top_scores = list(mongo.db.nivel_dos.find().sort('puntos', -1).limit(5))
+    top_scores = list(mongo.db.jugadores.find().sort('Puntuaje', -1).limit(5))
     for score in top_scores:
         score['_id'] = str(score['_id'])  # Convertir ObjectId a string
     return jsonify(top_scores), 200
